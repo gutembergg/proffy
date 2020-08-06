c909156a612dc25ec17a064efbdc3680dff8fad1
Gutembergue Mascarenhas <gmascarenhas3001@gmail.com>
2020-08-07 01:18:24 +0200

    created input component, teacher form css

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import PageHeaders from '../../components/PageHeaders'
 
+import './styles.css'
+
 function TeacherForm() {
   return (
-    <div>
-      <PageHeaders title="Incroyable que vous voulez donner des cours." />
+    <div id="page-teacher-form" className="container">
+      <PageHeaders
+        title="Incroyable que vous voulez donner des cours."
+        description="Premier pas c'est remplir le formulaire"
+      />
+      <main>
+        <fieldset>
+          <legend>Entrer vous données</legend>
+
+          <div className="input_block">
+            <label htmlFor="name">Name</label>
+            <input type="text" id="name" />
+          </div>
+
+          <div className="input_block">
+            <label htmlFor="avatar">Avatar</label>
+            <input type="text" id="avatar" />
+          </div>
+          <div className="input_block">
+            <label htmlFor="whatsapp">Whatsapp</label>
+            <input type="text" id="whatsapp" />
+          </div>
+        </fieldset>
+      </main>
     </div>
   )
 }
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -4,26 +4,16 @@ import PageHeaders from '../../components/PageHeaders'
 import TeacherItem from '../../components/TeacherItem'
 
 import './styles.css'
+import Input from '../../components/Inputs'
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeaders title="Voici les Proffys disponibles.">
         <form id="search-teacher">
-          <div className="input_block">
-            <label htmlFor="subject">Sujet</label>
-            <input type="text" id="subject" />
-          </div>
-
-          <div className="input_block">
-            <label htmlFor="week_day">Jour de la semaine</label>
-            <input type="text" id="week_day" />
-          </div>
-
-          <div className="input_block">
-            <label htmlFor="time">Horaire</label>
-            <input type="text" id="time" />
-          </div>
+          <Input name="subject" label="Sujet" />
+          <Input name="week_day" label="Jour de la semaine" />
+          <Input type="time" name="time" label="Horaire" />
         </form>
       </PageHeaders>
       <main>
