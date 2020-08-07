91d50f50a0f9468b5b063fd3583306cf104e1771
Gutembergue Mascarenhas <gmascarenhas3001@gmail.com>
2020-08-08 01:32:08 +0200

    inputs css

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,5 +1,9 @@
 import React from 'react'
 import PageHeaders from '../../components/PageHeaders'
+import Input from '../../components/Inputs'
+import WarningIcon from '../../assets/images/icons/warning.svg'
+import Textarea from '../../components/Textarea'
+import Select from '../../components/Select'
 
 import './styles.css'
 
@@ -14,20 +18,38 @@ function TeacherForm() {
         <fieldset>
           <legend>Entrer vous données</legend>
 
-          <div className="input_block">
-            <label htmlFor="name">Name</label>
-            <input type="text" id="name" />
-          </div>
-
-          <div className="input_block">
-            <label htmlFor="avatar">Avatar</label>
-            <input type="text" id="avatar" />
-          </div>
-          <div className="input_block">
-            <label htmlFor="whatsapp">Whatsapp</label>
-            <input type="text" id="whatsapp" />
-          </div>
+          <Input name="name" label="Name" />
+          <Input name="avatar" label="Avatar" />
+          <Input name="whatsapp" label="Whatsapp" />
         </fieldset>
+
+        <fieldset>
+          <legend>Vos cours</legend>
+
+          <Select
+            name="subject"
+            label="Langages de programmation"
+            options={[
+              { value: 'PHP', label: 'PHP' },
+              { value: 'Java', label: 'Java' },
+              { value: 'JavaScript', label: 'JavaScript' },
+              { value: 'Python', label: 'Python' },
+              { value: 'CMS', label: 'CMS' },
+              { value: 'HTML', label: 'HTML' }
+            ]}
+          />
+          <Textarea name="technologies" label="Technologies" />
+          <Input name="cost" label="Prix par heure" />
+        </fieldset>
+
+        <footer>
+          <p>
+            <img src={WarningIcon} alt="warning" />
+            Important ! <br />
+            Remplissez tout les champs
+          </p>
+          <button type="button">Valider</button>
+        </footer>
       </main>
     </div>
   )
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -5,14 +5,39 @@ import TeacherItem from '../../components/TeacherItem'
 
 import './styles.css'
 import Input from '../../components/Inputs'
+import Select from '../../components/Select'
 
 function TeacherList() {
   return (
     <div id="page-teacher-list" className="container">
       <PageHeaders title="Voici les Proffys disponibles.">
         <form id="search-teacher">
-          <Input name="subject" label="Sujet" />
-          <Input name="week_day" label="Jour de la semaine" />
+          <Select
+            name="subject"
+            label="Langages de programmation"
+            options={[
+              { value: 'PHP', label: 'PHP' },
+              { value: 'Java', label: 'Java' },
+              { value: 'JavaScript', label: 'JavaScript' },
+              { value: 'Python', label: 'Python' },
+              { value: 'CMS', label: 'CMS' },
+              { value: 'HTML', label: 'HTML' }
+            ]}
+          />
+          <Select
+            name="week_day"
+            label="Jour de la semaine"
+            options={[
+              { value: '0', label: 'Dimanche' },
+              { value: '1', label: 'Lundi' },
+              { value: '2', label: 'Mardi' },
+              { value: '3', label: 'Mercredi' },
+              { value: '4', label: 'Jeudi' },
+              { value: '5', label: 'Vendredi' },
+              { value: '6', label: 'Samedi' }
+            ]}
+          />
+
           <Input type="time" name="time" label="Horaire" />
         </form>
       </PageHeaders>
